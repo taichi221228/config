c3ef1a3b5bfbd9e2c21fc27ba0611c547c992bb6
taichi221228 <taichi221228@icloud.com>
2023-12-22 17:18:24 +0900

    Disable shellcheck warning for sourcing phpbrew bashrc
    This commit disables the shellcheck SC1090 warning for sourcing the phpbrew bashrc file in the zsh package. This ensures that the correct system environment is accessed when this package is used. It also prevents unnecessary warning messages when checking the script with shellcheck

--- zsh/main.zsh
+++ zsh/main.zsh
@@ -5,6 +5,7 @@
 
 source "$HOME/Library/Mobile Documents/com~apple~CloudDocs/Configs/zsh/package/package.zsh"
 
+# shellcheck disable=SC1090
 [[ -e ~/.phpbrew/bashrc ]] && source ~/.phpbrew/bashrc
 
 # Fig post block. Keep at the bottom of this file.
