556efb359bc608af435ff245f045596079351809
taichi221228 <taichi221228@icloud.com>
2024-01-16 23:40:05 +0900

    Remove unnecessary PHPBrew from 'main.zsh'

--- zsh/main.zsh
+++ zsh/main.zsh
@@ -5,8 +5,5 @@
 
 source "$HOME/Library/Mobile Documents/com~apple~CloudDocs/Configs/zsh/package/package.zsh"
 
-# shellcheck disable=SC1090
-[[ -e ~/.phpbrew/bashrc ]] && source ~/.phpbrew/bashrc
-
 # Fig post block. Keep at the bottom of this file.
 [[ -f "$HOME/.fig/shell/zshrc.post.zsh" ]] && builtin source "$HOME/.fig/shell/zshrc.post.zsh"
