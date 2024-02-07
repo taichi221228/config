7f320d644c00dc5e716eb691866fa2c1f67e8f83
taichi221228 <taichi221228@icloud.com>
2024-02-07 14:50:23 +0900

    Refine PATH command in pnpm.zsh script
    This commit revises the PATH command in the pnpm.zsh script by refining the case expression format. This adjustment improves readability and syncs code consistency with best practices.

--- zsh/package/pnpm.zsh
+++ zsh/package/pnpm.zsh
@@ -2,8 +2,8 @@
 
 export PNPM_HOME="$HOME/Library/pnpm"
 case ":$PATH:" in
-  *":$PNPM_HOME:"*) ;;
-  *) export PATH="$PNPM_HOME:$PATH" ;;
+*":$PNPM_HOME:"*) ;;
+*) export PATH="$PNPM_HOME:$PATH" ;;
 esac
 
 alias pnp="pnpm"
