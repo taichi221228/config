3ec3a03e4cfdb772c461e4fce917e6347b408c7f
taichi221228 <taichi221228@icloud.com>
2024-02-07 15:19:55 +0900

    Add aliases for Volta commands in zsh script
    The commit adds specific aliases for common Volta commands in volta.zsh script. This aims to simplify and streamline the use of Volta in a zsh terminal, enabling faster command entry and improving overall productivity.

--- zsh/package/volta.zsh
+++ zsh/package/volta.zsh
@@ -2,3 +2,11 @@
 
 export VOLTA_HOME="$HOME/.volta"
 export PATH="$VOLTA_HOME/bin:$PATH"
+
+alias vl="volta"
+alias vli="volta install"
+alias vlls="volta list"
+alias vlp="volta pin"
+alias vlr="volta run"
+alias vlu="volta uninstall"
+alias vlwh="volta which"
