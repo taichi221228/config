91c6e1f7f72311c7155f53baee45adb976a06c79
taichi221228 <taichi221228@icloud.com>
2024-02-16 11:33:45 +0900

    Remove and update functions in work.zsh; update .gitignore
    The file work.zsh was deleted and recreated with updated functions yamano:getimg and yamano:getjson. Additionally, an entry for package/work.zsh was added to .gitignore to prevent this file from being tracked in future commits.

--- zsh/package/work.zsh
+++ zsh/package/work.zsh
@@ -2,15 +2,15 @@
 
 function yamano:getimg() {
   cd "$L" && mkdir -p Yamano/_img && cd Yamano/_img || exit
-  scp -r taichi.fukuda@yamano-prod-prm-bat01:'/tmp/CooperateChirashiImage_*' .
+  scp -r taichi.fukuda@yamano-prd-prm-bat01:'/tmp/`CooperateChirashiImage_`*' .
 }
 
 function yamano:getjson() {
-    cd "$L" && mkdir -p Yamano/_json && cd Yamano/_json || exit
+  cd "$L" && mkdir -p Yamano/_json && cd Yamano/_json || exit
 
-    paste <(echo "0017300 0049490 0019150 0055040 0003760 0004880" | tr ' ' '\n') <(echo "1-北関東 2-南関東 3-東海 4-北陸 5-近畿 6-中四国" | tr ' ' '\n') | while read number company; do
-        mkdir "${company}" && cd "${company}" || exit
-        scp "yamano-stg-prm-web01:/mnt-chirashi/web/www/viewer/json/${number}.json" .
-        cd ..
-    done
+  paste <(echo "0017300 0049490 0019150 0055040 0003760 0004880" | tr ' ' '\n') <(echo "1-北関東 2-南関東 3-東海 4-北陸 5-近畿 6-中四国" | tr ' ' '\n') | while read number company; do
+    mkdir "${company}" && cd "${company}" || exit
+    scp "yamano-stg-chirashi-web01:/mnt/web/www/viewer/json/${number}.json" .
+    cd ..
+  done
 }
