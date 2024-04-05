a8ff1470753bb17bbc2fb8e3bd9960619e9e62d2
taichi221228 <taichi221228@icloud.com>
2024-04-05 17:46:55 +0900

    feat: Remove Docker-related aliases from zsh configuration
    This commit removes all Docker-related aliases present in the 'docker.zsh' configuration file. This change is due to these aliases no longer being necessary for the current shell session.

--- zsh/package/docker.zsh
+++ zsh/package/docker.zsh
@@ -3,15 +3,3 @@
 # Lima
 # export DOCKER_HOST=unix://${HOME}/.lima/docker/sock/docker.sock
 # export LIMA_INSTANCE=docker
-
-alias dc='docker'
-alias dcc='docker compose'
-alias dccd='docker compose down'
-alias dccs='docker compose start'
-alias dccst='docker compose stop'
-alias dccu='docker compose up'
-alias dce='docker exec'
-alias dcps='docker ps'
-alias dcr='docker run'
-alias dcs='docker start'
-alias dcst='docker stop'
