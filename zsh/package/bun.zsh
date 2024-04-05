e7151a13b2a6ed722f836d9209bdb6bf9e7fbdee
taichi221228 <taichi221228@icloud.com>
2024-04-05 17:45:20 +0900

    feat: Remove redundant bun aliases in bun.zsh file
    This commit simplifies the codebase by removing a series of aliases related to bun commands in the bun.zsh file. These aliases include those for commands such as create, run, init, list, and uninstall.

--- zsh/package/bun.zsh
+++ zsh/package/bun.zsh
@@ -4,25 +4,3 @@
 
 export BUN_INSTALL="$HOME/.bun"
 export PATH="$BUN_INSTALL/bin:$PATH"
-
-alias bn='bun'
-alias bnc='bun create'
-alias bnb='bun run build'
-alias bnd='bun run dev'
-alias bndp='bun run deploy'
-alias bnf='bun run format'
-alias bni='bun i'
-alias bnid='bun i -D'
-alias bnig='bun i -g'
-alias bnit='bun init'
-alias bnity='bun init -y'
-alias bnl='bun run lint'
-alias bnlf='bun run lint.fix'
-alias bnlff='bun run lint.fix && bun run format'
-alias bnls='bun list'
-alias bnlsg='bun list -g'
-alias bnr='bun run'
-alias bns='bun start'
-alias bnt='bun test'
-alias bnun='bun uninstall'
-alias bnug='bun -g uninstall'
