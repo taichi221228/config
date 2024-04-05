63c25a387b787bc081c8cde64b7eceb9310082cd
taichi221228 <taichi221228@icloud.com>
2024-04-05 17:50:45 +0900

    feat: Remove functions from work.zsh script
    The work.zsh script has been streamlined and cleaned up by removing two functions, `yamano:getimg()` and `yamano:getjson()`.

--- zsh/package/work.zsh
+++ zsh/package/work.zsh
@@ -1,16 +1,1 @@
 #!/usr/bin/env zsh
-
-function yamano:getimg() {
-  cd "$L" && mkdir -p Yamano/_img && cd Yamano/_img || exit
-  scp -r taichi.fukuda@yamano-prd-prm-bat01:'/tmp/`CooperateChirashiImage_`*' .
-}
-
-function yamano:getjson() {
-  cd "$L" && mkdir -p Yamano/_json && cd Yamano/_json || exit
-
-  paste <(echo "0017300 0049490 0019150 0055040 0003760 0004880" | tr ' ' '\n') <(echo "1-北関東 2-南関東 3-東海 4-北陸 5-近畿 6-中四国" | tr ' ' '\n') | while read number company; do
-    mkdir "${company}" && cd "${company}" || exit
-    scp "yamano-stg-chirashi-web01:/mnt/web/www/viewer/json/${number}.json" .
-    cd ..
-  done
-}
