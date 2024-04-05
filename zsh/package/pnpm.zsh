06d89033a5d478b409e5cb6b9e45352f1da5ee1a
taichi221228 <taichi221228@icloud.com>
2024-04-05 17:49:06 +0900

    feat: Remove pnpm aliases from zsh script
    The commit deletes a series of alias commands for pnpm within the pnpm.zsh file.

--- zsh/package/pnpm.zsh
+++ zsh/package/pnpm.zsh
@@ -5,25 +5,3 @@ case ":$PATH:" in
 *":$PNPM_HOME:"*) ;;
 *) export PATH="$PNPM_HOME:$PATH" ;;
 esac
-
-alias pnp='pnpm'
-alias pnpc='pnpm create'
-alias pnpb='pnpm run build'
-alias pnpd='pnpm run dev'
-alias pnpdp='pnpm run deploy'
-alias pnpf='pnpm run format'
-alias pnpi='pnpm i'
-alias pnpid='pnpm i -D'
-alias pnpig='pnpm i -g'
-alias pnpit='pnpm init'
-alias pnpity='pnpm init -y'
-alias pnpl='pnpm run lint'
-alias pnplf='pnpm run lint.fix'
-alias pnplff='pnpm run lint.fix && pnpm run format'
-alias pnpls='pnpm list'
-alias pnplsg='pnpm list -g'
-alias pnpr='pnpm run'
-alias pnps='pnpm start'
-alias pnpt='pnpm test'
-alias pnpun='pnpm uninstall'
-alias pnpug='pnpm -g uninstall'
