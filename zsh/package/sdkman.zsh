d704a1669f049f5a7275df461e3782ff7d7a21d6
taichi221228 <taichi221228@icloud.com>
2024-06-29 07:18:25 +0900

    feat: Delete SDKMan package
    Deleted init.zsh and sdkman.zsh under sdkman package. Removed their sourcing in package.zsh as well. This is a clean-up operation which removes unused or unnecessary scripts from the package hierarchy.

--- zsh/package/sdkman.zsh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env zsh
-
-#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
-export SDKMAN_DIR="$HOME/.sdkman"
-[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
