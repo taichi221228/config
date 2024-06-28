9b715f7f1bca1f85dcbd71ebdad87d9d521492f8
taichi221228 <taichi221228@icloud.com>
2024-06-29 07:42:29 +0900

    feat: Remove Fig pre and post block sourcing in main.zsh
    This commit removes the inclusion of Fig pre and post block in the main.zsh file. These changes streamline the file and eliminates unnecessary sourcing previously required for the Fig shell completion framework.

--- zsh/main.zsh
+++ zsh/main.zsh
@@ -1,9 +1,3 @@
 #!/usr/bin/env zsh
 
-# Fig pre block. Keep at the top of this file.
-[[ -f "$HOME/.fig/shell/zshrc.pre.zsh" ]] && builtin source "$HOME/.fig/shell/zshrc.pre.zsh"
-
 source "$HOME/Library/Mobile Documents/com~apple~CloudDocs/Configs/general/zsh/package/package.zsh"
-
-# Fig post block. Keep at the bottom of this file.
-[[ -f "$HOME/.fig/shell/zshrc.post.zsh" ]] && builtin source "$HOME/.fig/shell/zshrc.post.zsh"
