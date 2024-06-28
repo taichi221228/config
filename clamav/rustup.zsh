8431f39f1a151c4f3a37dfd57310cddc1121d103
taichi221228 <taichi221228@icloud.com>
2024-06-29 07:16:20 +0900

    feat: Delete ClamAV package
    All Shell script files related to the installation and configuration of ClamAV (an open-source antivirus engine) have been removed. The project has decided to cease reliance on the ClamAV library, thus eliminating these scripts helps to reduce complexity and allows for easier maintenance going forward.

--- clamav/rustup.zsh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env zsh
-
-# download
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
--- zsh/package/clamav.zsh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-
-export PATH="/usr/local/cmake/bin:$PATH"
-source "$HOME/.cargo/env"
-export PATH="/usr/local/keyconfig/bin:$PATH"
-export PATH="/usr/local/check/bin:$PATH"
-export PATH="/usr/local/libressl/bin:$PATH"
